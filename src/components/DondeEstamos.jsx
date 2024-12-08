a7ea7e7d5baac7effb126a09d02a547ed9547636
Juanjo Ruiz <juanjoserusa@gmail.com>
2024-12-08 10:03:46 +0100

    New changes

--- src/components/DondeEstamos.jsx
+++ src/components/DondeEstamos.jsx
@@ -2,38 +2,39 @@ import React from 'react';
 import "./DondeEstamos.css";
 
 const DondeEstamos = () => {
-  return (
-    <section className="donde-estamos-container container-fluid">
-      <div className="container donde-estamos-content">
-        <div className="row">
-          {/* Sección de texto sobre la ubicación */}
-          <div className="col-12 col-md-6 text-content">
-            <h2>¿Dónde estamos?</h2>
-            <p>Nos encontramos en la siguiente dirección:</p>
-            <p>
-              <strong>C/Pablo Coso Calero, 2, oficina 2. BORMUJOS (Sevilla)</strong> 
-              <br />
-              (Entrada por calle Albaicín 2A. En el edificio de la Notaría, junto al supermercado MAS)
-            </p>
-          </div>
+    return (
+        <section className="donde-estamos-container container-fluid">
+            <div className="container donde-estamos-content">
+                <div className="row">
+                    {/* Sección de texto sobre la ubicación */}
+                    <div className="col-12 text-content">
+                        <h2>¿Dónde estamos?</h2>
+                        <p>Nos encontramos en la siguiente dirección:</p>
+                        <p>
+                            <strong> C/ Albaicín, nº 2ª, oficina 2. Bormujos.(Sevilla)</strong>
+                            <br/>
+                            (Edificio de la NOTARÍA, junto a SUPERMERCADO MAS)
+                        </p>
+                    </div>
 
-          {/* Sección del mapa con Google Maps */}
-          <div className="col-12 col-md-6 map-content">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.8776606543013!2d-6.077678024340147!3d37.37375273982654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126da3acfd4169%3A0x5586b6677c8f4e6b!2sC.%20Pablo%20Coso%20Calero%2C%202%2C%2041930%20Bormujos%2C%20Sevilla%2C%20Espa%C3%B1a!5e0!3m2!1ses!2sus!4v1696522957415!5m2!1ses!2sus"
-              width="100%"
-              height="300"
-              style={{ border: 0, borderRadius: '15px' }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Mapa ubicación"
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+                    {/* Sección del mapa con Google Maps */}
+                    <div className="col-12 map-content">
+                        <iframe
+                            // 37.36811889360275, -6.080111149167616
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1585.4598680280385!2d-6.081173396599333!3d37.368076028904696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd12133af5ea8481%3A0xbe6093406c7ced7f!2sC.%20Albaic%C3%ADn%2C%202A%2C%20Oficina%202%2C%2041930%20Bormujos%2C%20Sevilla!5e0!3m2!1ses!2ses!4v1733647146779!5m2!1ses!2ses"
+                            width="100%"
+                            height="400"
+                            style={{border: 0, borderRadius: '15px'}}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Mapa ubicación"
+                        ></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default DondeEstamos;
--- src/pages/SobreMi.jsx
+++ src/pages/SobreMi.jsx
@@ -2,44 +2,48 @@ import React from 'react';
 import './SobreMi.css'; // Archivo CSS actualizado
 
 const SobreMi = () => {
-  return (
-    <section className="sobre-mi-container container-fluid">
-      <div className="container sobre-mi-content">
-        <div className="row">
-          {/* Imagen */}
-          <div className="col-md-6 sobre-mi-image">
-            <img
-              src="./src/assets/img/fi.jpg" // Cambia esta ruta por la de tu imagen real
-              alt="Foto de Fátima"
-              className="img-responsive"
-            />
-          </div>
-          {/* Texto de descripción */}
-          <div className="col-md-6 sobre-mi-text">
-            <h2>Conócenos</h2>
-            <p>
-              ¡Hola! Soy Fátima, la responsable de Saberes, además de la profe. Mi profesión
-              es por vocación: desde niña quise ser maestra.
-            </p>
-            <p>
-              Con más de quince años de experiencia, he decidido dar un salto y montar mi propia
-              academia, donde poder ayudar a cada alumno/a a encontrar sus puntos fuertes y superar sus obstáculos.
-            </p>
-            <p>
-              “Los límites sólo existen en tu cabeza” y “aunque lo veas negro alcanzarás la cima”
-              son dos frases que escuchan mucho mis alumnos. El hacerles ver que si se lo
-              proponen, pueden; y que superar obstáculos es gratificante, aunque requiere de un
-              esfuerzo por su parte; son dos de mis claves.
-            </p>
-            <p>
-              Las demás las van poniendo ellos con su forma de ser, y por ello la enseñanza aquí
-              es individualizada: cada uno aprende de una manera diferente, y nos adaptamos a ella.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="sobre-mi-container container-fluid">
+            <div className="container sobre-mi-content">
+                <div className="row">
+                    {/* Imagen */}
+                    <div className="col-md-6 sobre-mi-image">
+                        <img
+                            src="./src/assets/img/fi.jpg" // Cambia esta ruta por la de tu imagen real
+                            alt="Foto de Fátima"
+                            className="img-responsive"
+                        />
+                    </div>
+                    {/* Texto de descripción */}
+                    <div className="col-md-6 sobre-mi-text">
+                        <h2>Conócenos</h2>
+                        <p>
+                            ¡Hola! Soy Fátima, la responsable y la profesora de SABERES.
+                        </p>
+                        <p>
+                            Mi trabajo es mi pasión, con más de 10 años de experiencia, decidí dar un
+                            salto y abrir un espacio nuevo donde poder ayudar a cada alumno/a a
+                            <strong>mejorar en sus resultados académicos</strong>: aprender a pensar, aprender a
+                            organizarse, aprender a estudiar; pero también un lugar donde pueden
+                            evolucionar y crecer como persona: ser ellos mismos, crear lazos de amistad
+                            con otros alumnos y <strong>desarrollar su confianza y su autoestima</strong>.
+                        </p>
+                        <p>
+                            <strong>“Los límites sólo existen en tu cabeza”</strong> y <strong>“aunque lo veas negro
+                            alcanzarás
+                            la cima”</strong> son dos frases que escuchan mucho mis alumnos. Hacerles ver que
+                            si se lo proponen, pueden; y que superar obstáculos es gratificante, aunque
+                            requiere de un esfuerzo por su parte; son dos de mis claves.
+                        </p>
+                        <p>
+                            Las demás las van poniendo ellos con su forma de ser: cada uno aprende de
+                            una manera diferente, y nos adaptamos a ella.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default SobreMi;
